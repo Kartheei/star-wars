73ce9c9c8066c804b90d47ac0f5ca2717793dd08
Karthick_KS <30871668+Kartheei@users.noreply.github.com>
2024-09-17 12:54:47 -0400

    Initial Commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { useQuery } from '@apollo/client';
+// import { GET_FILMS } from '../../graphql/queries';
+
+
+const Home: React.FC = () => {
+
+}
+
+// export default Home;
+
+export {
+    Home
+};
