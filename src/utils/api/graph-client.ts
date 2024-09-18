892dcb3e0298596afe1aaa674fc37b5e85b93c9f
Karthick_KS <30871668+Kartheei@users.noreply.github.com>
2024-09-18 16:44:07 -0400

    design change

--- src/utils/api/graph-client.ts
+++ src/utils/api/graph-client.ts
@@ -1,8 +1,76 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import {
+    ApolloClient,
+    createHttpLink,
+    InMemoryCache,
+    ApolloLink
+} from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+import { RetryLink } from "@apollo/client/link/retry";
+
+
+const httpLink = createHttpLink({
+    uri: "https://swapi-graphql.netlify.app/.netlify/functions/index",
+});
+
+const errorLink = onError(({ graphQLErrors, networkError, operation, forward }) => {
+    if (graphQLErrors) {
+        graphQLErrors.forEach(({ message, locations, path }) => {
+            console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`);
+        });
+
+        return;
+    }
+
+    if (networkError) {
+        console.log(`[Network error]: ${networkError}`);
+    }
+
+    return forward(operation);
+});
+
+const retryLink = new RetryLink({
+    attempts: {
+        max: 3,
+        retryIf: (error, _operation) => !!error && error.statusCode !== 400,
+    },
+    delay: {
+        initial: 300,
+        max: 2000,
+        jitter: true
+    }
+});
+
+const link = ApolloLink.from([errorLink, retryLink, httpLink]);
+
+const TTL = 10 * 1000;
+
+const cache = new InMemoryCache({
+    typePolicies: {
+        Query: {
+            fields: {
+                _: {
+                    read(existing) {
+                        if (existing && Date.now() - existing.__timestamp < TTL) {
+                            return existing.data;
+                        }
+                        return undefined;
+                    },
+                    merge(existing, incoming) {
+                        return {
+                            ...incoming,
+                            __timestamp: Date.now(),
+                            data: incoming
+                        };
+                    }
+                }
+            }
+        }
+    }
+});
 
 const client = new ApolloClient({
-    uri: "https://swapi-graphql.netlify.app/.netlify/functions/index",
-    cache: new InMemoryCache(),
+    link,
+    cache
 });
 
 export { client };
